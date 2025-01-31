7fb007ba9f3a91b1d982c887279a33f0738f0d8a
MMahdiSetak <m.mahdi.setak@gmail.com>
2025-01-31 17:36:40 +0330

    Refactor Logger to handle dynamic model-specific log paths

--- model/log.py
+++ model/log.py
@@ -8,9 +8,9 @@ from torchsummary import summary
 
 
 class Logger:
-    def __init__(self):
+    def __init__(self, model_name):
         self.time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M")
-        self.log_dir = os.path.join("log/gan_class", self.time_stamp)
+        self.log_dir = os.path.join(f"log/{model_name}", self.time_stamp)
         self.writer = SummaryWriter(log_dir=self.log_dir)
 
     def save_model_metadata(self, model, input, name, batch_size):
