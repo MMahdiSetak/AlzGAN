bac1109f3dd0684538f51f379a39a7e4b91de768
MMahdiSetak <m.mahdi.setak@gmail.com>
2025-01-31 17:38:42 +0330

    Refactor data handling and logging in GAN classifier training.

--- gan_class_large.py
+++ gan_class_large.py
@@ -1,51 +1,19 @@
-import io
 import os
-import sys
-from datetime import datetime
 
 import h5py
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.tensorboard import SummaryWriter
 from torchsummary import summary
 from tqdm import tqdm
+
+from model.dataloader import data_generator
+from model.log import Logger
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
 mri_target_shape = (1, 160, 200, 180)
-
-
-def pair_data_generator(hdf5_file, batch_size, split):
-    with h5py.File(hdf5_file, 'r') as file:
-        if split == 'train':
-            mri_images = file['mri_train']
-            labels = file['label_train']
-        elif split == 'val':
-            mri_images = file['mri_val']
-            labels = file['label_val']
-        else:
-            mri_images = file['mri_test']
-            labels = file['label_test']
-        n = len(labels)
-        indices = np.arange(n)
-        while True:
-            for i in range(0, n, batch_size):
-                end = min(i + batch_size, n)
-                batch_indices = indices[i:end]  # Get batch indices
-
-                if len(batch_indices) < batch_size:
-                    r = np.random.randint(0, n - batch_size)
-                    additional_indices = indices[r:r + (batch_size - len(batch_indices))]
-                    batch_indices = np.concatenate((additional_indices, batch_indices))
-
-                batch_mri = mri_images[batch_indices]
-                batch_label = labels[batch_indices]
-
-                batch_mri = torch.Tensor(batch_mri / 256).unsqueeze(1).to(device)  # Add channel dimension
-                yield batch_mri, batch_label
 
 
 class Generator(nn.Module):
@@ -165,15 +133,9 @@ optimizer_c = optim.Adam(classifier.parameters(), lr=0.00003, betas=(0.5, 0.999)
 
 classification_loss = nn.CrossEntropyLoss()
 
-time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M")
-log_dir = os.path.join("log/gan_class", time_stamp)
-writer = SummaryWriter(log_dir=log_dir)
-
 
 def train_gan_class(generator, classifier, data_generator, val_data_generator, epochs=100, steps_per_epoch=100,
                     steps_per_epoch_val=50):
-    model_path = f"model/GAN_class/{time_stamp}"
-    os.makedirs(model_path, exist_ok=True)
     best_acc = 0
     scheduler_g = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_g, T_max=100, eta_min=1e-6)
     scheduler_c = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_c, T_max=100, eta_min=1e-6)
@@ -250,10 +212,10 @@ def train_gan_class(generator, classifier, data_generator, val_data_generator, e
                 f"Train Loss: {epoch_train_loss:.4f}, Train Accuracy: {epoch_train_accuracy:.2f}, "
                 f"Val Loss: {epoch_val_loss:.4f}, Val Accuracy: {epoch_val_accuracy:.2f}"
             )
-            writer.add_scalar("Loss/train", epoch_train_loss, epoch + 1)
-            writer.add_scalar("Loss/val", epoch_val_loss, epoch + 1)
-            writer.add_scalar("Acc/train", epoch_train_accuracy, epoch + 1)
-            writer.add_scalar("Acc/val", epoch_val_accuracy, epoch + 1)
+            logger.writer.add_scalar("Loss/train", epoch_train_loss, epoch + 1)
+            logger.writer.add_scalar("Loss/val", epoch_val_loss, epoch + 1)
+            logger.writer.add_scalar("Acc/train", epoch_train_accuracy, epoch + 1)
+            logger.writer.add_scalar("Acc/val", epoch_val_accuracy, epoch + 1)
 
             if epoch_val_accuracy > best_acc:
                 best_acc = epoch_val_accuracy
@@ -265,18 +227,8 @@ def train_gan_class(generator, classifier, data_generator, val_data_generator, e
                     "optimizer_c_state_dict": optimizer_c.state_dict(),
                     "best_acc": best_acc,
                 }
-                torch.save(checkpoint, os.path.join(model_path, "gan_class.pth"))
+                torch.save(checkpoint, os.path.join(logger.log_dir, "gan_class.pth"))
                 print(f"New best model saved with accuracy: {best_acc:.2f}")
-
-
-def save_model_metadata(model, input, name, batch_size):
-    with open(f"{log_dir}/{name}.txt", "w") as f:
-        buffer = io.StringIO()
-        sys.stdout = buffer
-        summary(model, input, batch_size=batch_size)
-        sys.stdout = sys.__stdout__
-        out = buffer.getvalue()
-        f.write(out)
 
 
 hdf5_file = 'dataset/mri_label.hdf5'
@@ -287,12 +239,13 @@ with h5py.File(hdf5_file, 'r') as file:
 batch_size = 32
 summary(generator, mri_target_shape, batch_size=batch_size)
 
-save_model_metadata(generator, mri_target_shape, "generator", batch_size)
-save_model_metadata(classifier, (2 * 1024, 20, 25, 22), "classifier", batch_size)
+logger = Logger("gan_class")
+logger.save_model_metadata(generator, mri_target_shape, "generator", batch_size)
+logger.save_model_metadata(classifier, (2 * 1024, 20, 25, 22), "classifier", batch_size)
 
 steps_per_epoch = train_size // batch_size
 steps_per_epoch_val = val_size // batch_size
-train_data_generator = pair_data_generator(hdf5_file, batch_size, "train")
-val_data_generator = pair_data_generator(hdf5_file, batch_size, "val")
+train_data_generator = data_generator(hdf5_file, batch_size, "train", pet=False, label=True)
+val_data_generator = data_generator(hdf5_file, batch_size, "val", pet=False, label=True)
 train_gan_class(generator, classifier, train_data_generator, val_data_generator, epochs=500,
                 steps_per_epoch=steps_per_epoch, steps_per_epoch_val=steps_per_epoch_val)
