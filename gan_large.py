db94c1d3cbbd15db090bb0b0ca118781b6126bda
MMahdiSetak <m.mahdi.setak@gmail.com>
2025-01-31 17:37:42 +0330

    Refactor GAN training pipeline and integrate Logger system.

--- gan_large.py
+++ gan_large.py
@@ -1,99 +1,65 @@
 import math
 import os
-from datetime import datetime
 
 import h5py
 import matplotlib.pyplot as plt
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.tensorboard import SummaryWriter
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from tqdm import tqdm
 from torchsummary import summary
+from tqdm import tqdm
+
+from model.dataloader import data_generator
+from model.log import Logger
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
-
-mri_target_shape = (1, 160, 192, 192)
-pet_target_shape = (1, 35, 128, 128)
-
-
-def pair_data_generator(hdf5_file, batch_size, split):
-    with h5py.File(hdf5_file, 'r') as file:
-        if split == 'train':
-            mri_images = file['mri_train']
-            pet_images = file['pet_train']
-        elif split == 'val':
-            mri_images = file['mri_val']
-            pet_images = file['pet_val']
-        else:
-            mri_images = file['mri_test']
-            pet_images = file['pet_test']
-        n = len(mri_images)
-        indices = np.arange(n)
-        while True:
-            for i in range(0, n, batch_size):
-                end = min(i + batch_size, n)
-                batch_indices = indices[i:end]  # Get batch indices
-
-                if len(batch_indices) < batch_size:
-                    # Randomly sample additional indices to pad the batch
-                    r = np.random.randint(0, n - batch_size)
-                    additional_indices = indices[r:r + (batch_size - len(batch_indices))]
-                    batch_indices = np.concatenate((additional_indices, batch_indices))
-
-                # Retrieve data for the batch
-                batch_mri = mri_images[batch_indices]
-                batch_pet = pet_images[batch_indices]
-
-                batch_mri = torch.Tensor(batch_mri / 256).unsqueeze(1).to(device)  # Add channel dimension
-                batch_pet = torch.Tensor(batch_pet / 256).unsqueeze(1).to(device)
-                yield batch_mri, batch_pet
+logger = Logger("gan")
+mri_target_shape = (1, 160, 200, 180)
+pet_target_shape = (1, 100, 140, 96)
 
 
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
-
         self.encoder = nn.Sequential(
             nn.Conv3d(1, 128, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm3d(128),
             nn.ReLU(inplace=True),
-            nn.Dropout3d(0.2),
+            nn.Dropout3d(0.1),
 
             nn.Conv3d(128, 256, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm3d(256),
             nn.ReLU(inplace=True),
-            nn.Dropout3d(0.2),
+            nn.Dropout3d(0.1),
 
             nn.Conv3d(256, 512, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm3d(512),
             nn.ReLU(inplace=True),
-            nn.Dropout3d(0.2),
+            nn.Dropout3d(0.1),
 
             nn.Conv3d(512, 1024, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm3d(1024),
             nn.ReLU(inplace=True),
-            nn.Dropout3d(0.2),
+            nn.Dropout3d(0.1),
         )
 
         self.bottleneck = nn.Sequential(
             nn.Conv3d(1024, 1024, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm3d(1024),
             nn.LeakyReLU(0.2),
-            nn.Dropout3d(0.3),
+            nn.Dropout3d(0.2),
 
             nn.Conv3d(1024, 1024, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm3d(1024),
             nn.LeakyReLU(0.2),
-            nn.Dropout3d(0.3),
+            nn.Dropout3d(0.2),
 
             nn.Conv3d(1024, 1024, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm3d(1024),
             nn.LeakyReLU(0.2),
-            nn.Dropout3d(0.3),
+            nn.Dropout3d(0.2),
         )
 
         self.decoder = nn.Sequential(
@@ -121,7 +87,7 @@ class Generator(nn.Module):
         x = self.bottleneck(x)
         x = self.decoder(x)
 
-        return x[:, :, :35, :128, :128]
+        return x[:, :, :100, :140, :96]
 
 
 class Discriminator(nn.Module):
@@ -188,10 +154,6 @@ scheduler = ReduceLROnPlateau(optimizer_g, 'min', patience=3, factor=0.5)
 
 criterion = nn.BCELoss()
 
-time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M")
-log_dir = os.path.join("log/gan", time_stamp)
-writer = SummaryWriter(log_dir=log_dir)
-
 
 def visualize_progress(img, file_name):
     center_slices = [dim // 2 for dim in img.shape]
@@ -205,7 +167,9 @@ def visualize_progress(img, file_name):
         ax.imshow(slice_img, cmap='gray')
         ax.set_title(title)
 
-    plt.savefig(f"model/GAN/{time_stamp}/{file_name}.png")
+    model_dir = f"{logger.log_dir}/model"
+    os.makedirs(model_dir, exist_ok=True)
+    plt.savefig(f"{model_dir}/{file_name}.png")
     plt.close(fig)
 
 
@@ -213,8 +177,6 @@ def train_gan(generator, discriminator, data_generator, val_data_generator, epoc
               steps_per_epoch=100, steps_per_epoch_val=50):
     valid = torch.ones((batch_size, *discriminator(torch.randn(1, *pet_target_shape).to(device)).shape[1:])).to(device)
     fake = torch.zeros((batch_size, *discriminator(torch.randn(1, *pet_target_shape).to(device)).shape[1:])).to(device)
-    model_path = f"model/GAN/{time_stamp}"
-    os.makedirs(model_path, exist_ok=True)
 
     best_psnr = -float('inf')  # Initialize best PSNR to negative infinity
 
@@ -246,14 +208,22 @@ def train_gan(generator, discriminator, data_generator, val_data_generator, epoc
         epoch_train_gloss /= steps_per_epoch
         epoch_train_dloss /= steps_per_epoch
 
-        writer.add_scalar("D_Loss", epoch_train_dloss, epoch + 1)
-        writer.add_scalar("G_Loss", epoch_train_gloss, epoch + 1)
+        logger.writer.add_scalar("D_Loss", epoch_train_dloss, epoch + 1)
+        logger.writer.add_scalar("G_Loss", epoch_train_gloss, epoch + 1)
         psnr = evaluate(generator, val_data_generator, steps_per_epoch_val)
-        writer.add_scalar("PSNR", psnr, epoch + 1)
+        logger.writer.add_scalar("PSNR", psnr, epoch + 1)
 
         if psnr > best_psnr:
             best_psnr = psnr
-            torch.save(generator.state_dict(), f"{model_path}/best_generator_epoch_{epoch + 1}_psnr_{psnr:.2f}.pth")
+            checkpoint = {
+                "epoch": epoch + 1,
+                "generator_state_dict": generator.state_dict(),
+                "discriminator_state_dict": discriminator.state_dict(),
+                "optimizer_g_state_dict": optimizer_g.state_dict(),
+                "optimizer_d_state_dict": optimizer_d.state_dict(),
+                "best_psnr": best_psnr,
+            }
+            torch.save(checkpoint, os.path.join(logger.log_dir, "gan.pth"))
             print(f"Saved model with PSNR: {psnr:.2f}")
         visualize_progress(fake_pet[0][0].cpu().detach().numpy(), f"{epoch:03d}_fake_pet")
         visualize_progress(real_mri[0][0].cpu().detach().numpy(), f"{epoch:03d}_real_mri")
@@ -266,9 +236,13 @@ with h5py.File(hdf5_file, 'r') as file:
     val_size = len(file['label_val'])
     test_size = len(file['label_test'])
 batch_size = 20
+
+logger.save_model_metadata(generator, mri_target_shape, "generator", batch_size)
+logger.save_model_metadata(discriminator, pet_target_shape, "discriminator", batch_size)
+
 steps_per_epoch = math.ceil(train_size / batch_size)
 steps_per_epoch_val = math.ceil(val_size / batch_size)
-train_data_generator = pair_data_generator(hdf5_file, batch_size, "train")
-val_data_generator = pair_data_generator(hdf5_file, batch_size, "val")
-train_gan(generator, discriminator, train_data_generator, val_data_generator, epochs=300, batch_size=batch_size,
+train_data_generator = data_generator(hdf5_file, batch_size, "train", pet=True, label=False)
+val_data_generator = data_generator(hdf5_file, batch_size, "val", pet=True, label=False)
+train_gan(generator, discriminator, train_data_generator, val_data_generator, epochs=500, batch_size=batch_size,
           steps_per_epoch=steps_per_epoch, steps_per_epoch_val=steps_per_epoch_val)
