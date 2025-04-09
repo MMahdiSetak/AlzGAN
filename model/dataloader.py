d2a721e510c9de9fb26bb677d86fa7d9a2ae6d13
MMahdiSetak <m.mahdi.setak@gmail.com>
2025-04-09 11:45:40 +0330

    add mri generator to dataloader

--- model/dataloader.py
+++ model/dataloader.py
@@ -87,3 +87,28 @@ class DataLoader:
                     batch_pet = pet_images[batch_indices]
                     batch_pet = torch.Tensor(batch_pet / 256).unsqueeze(1).to(device)
                     yield batch_pet
+
+    def mri_generator(self, batch_size, split):
+        split_dict = {
+            'train': 'mri_train',
+            'val': 'mri_val',
+            'test': 'mri_test'
+        }
+        with h5py.File(self.data_path, 'r') as file:
+            mri_images = file[split_dict[split]]
+            n = len(mri_images)
+            indices = np.arange(n)
+            while True:
+                for i in range(0, n, batch_size):
+                    end = min(i + batch_size, n)
+                    batch_indices = indices[i:end]  # Get batch indices
+
+                    if len(batch_indices) < batch_size:
+                        # Randomly sample additional indices to pad the batch
+                        r = np.random.randint(0, n - batch_size)
+                        additional_indices = indices[r:r + (batch_size - len(batch_indices))]
+                        batch_indices = np.concatenate((additional_indices, batch_indices))
+
+                    batch_pet = mri_images[batch_indices]
+                    batch_pet = torch.Tensor(batch_pet / 256).unsqueeze(1).to(device)
+                    yield batch_pet
