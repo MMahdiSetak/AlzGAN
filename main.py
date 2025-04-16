cd90902d3f45ed34d3019f4bfbb2ecd71f93ba9b
MMahdiSetak <m.mahdi.setak@gmail.com>
2025-04-16 22:12:00 +0330

    add optuna for cvit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #
 # get_patch_indices()
 
+
 # from train.vq_gan_mri import run
 #
 # run()
@@ -10,10 +11,22 @@
 #
 # run()
 
-from train.cvit import run
-
-run()
+# from train.cvit import run
+#
+# run()
 
 # from dataset import mri_data_paths, create_mri_pet_label_dataset, pet_data_path
 #
 # create_mri_pet_label_dataset(mri_data_paths, pet_data_path)
+
+import optuna
+
+from train.opt_hparm import objective
+
+# Create the Optuna study and run the optimization
+study = optuna.create_study(direction="maximize")
+study.optimize(objective, n_trials=50)
+
+# Get the best hyperparameters
+best_params = study.best_params
+print(f"Best Hyperparameters: {best_params}")
---
+++ train/opt_hparm.py
@@ -0,0 +1,50 @@
+import optuna
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
+from torch import nn
+from torch.utils.data import DataLoader
+
+from model.cvit.model import SegmentTransformer
+from model.dataloader import MRIDataset
+
+
+def objective(trial):
+    # Suggest hyperparameters
+    embedding_size = trial.suggest_int('embedding_size', 64, 512)
+    dropout_rate = trial.suggest_float('dropout_rate', 0.1, 0.9)
+    batch_size = 256
+    num_workers = 3
+
+    # Define the model with the suggested hyperparameters
+    model = SegmentTransformer(embedding_size=embedding_size, dropout_rate=dropout_rate)
+    model.classification_loss = nn.CrossEntropyLoss()
+
+    # Define trainer
+    trainer = pl.Trainer(
+        max_epochs=50,
+        accelerator="auto",
+        logger=TensorBoardLogger(save_dir="./log", name="cvit_optuna"),
+        val_check_interval=1.0,
+        precision='16-mixed',
+        gradient_clip_val=1.0,
+    )
+
+    # Set up the DataLoader objects for train, val, and test sets
+    train_loader = DataLoader(
+        dataset=MRIDataset('dataset/mri_label_v3.hdf5', 'train'),
+        batch_size=batch_size, num_workers=num_workers, shuffle=False, drop_last=True
+    )
+    val_loader = DataLoader(
+        dataset=MRIDataset('dataset/mri_label_v3.hdf5', 'val'),
+        batch_size=batch_size, num_workers=num_workers, shuffle=False, drop_last=True
+    )
+
+    # Train the model
+    trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+
+    # Return the validation accuracy as the objective to maximize
+    val_accuracy = trainer.callback_metrics['val_accuracy']
+    return val_accuracy
+
+
+
