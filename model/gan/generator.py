277cf6c5769bf3c53246a80fc030fd05fcd2af19
MMahdiSetak <m.mahdi.setak@gmail.com>
2025-04-18 14:40:48 +0330

    set hparam for Generator

--- model/gan/generator.py
+++ model/gan/generator.py
@@ -2,64 +2,55 @@ import torch.nn as nn
 
 
 class Generator(nn.Module):
-    def __init__(self):
+    def __init__(self,
+                 base_nc: int = 128,
+                 enc_dropout: float = 0.1,
+                 bottle_dropout: float = 0.2,
+                 dec_dropout: float = 0.1,
+                 n_bottleneck: int = 3):
         super(Generator, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Conv3d(1, 128, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(128),
-            nn.ReLU(inplace=True),
-            nn.Dropout3d(0.1),
+            nn.Conv3d(1, base_nc, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm3d(base_nc),
+            nn.ReLU(inplace=True), nn.Dropout3d(enc_dropout),
 
-            nn.Conv3d(128, 256, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(256),
-            nn.ReLU(inplace=True),
-            nn.Dropout3d(0.1),
+            nn.Conv3d(base_nc, base_nc * 2, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm3d(base_nc * 2),
+            nn.ReLU(inplace=True), nn.Dropout3d(enc_dropout),
 
-            nn.Conv3d(256, 512, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(512),
-            nn.ReLU(inplace=True),
-            nn.Dropout3d(0.1),
+            nn.Conv3d(base_nc * 2, base_nc * 4, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm3d(base_nc * 4),
+            nn.ReLU(inplace=True), nn.Dropout3d(enc_dropout),
 
-            nn.Conv3d(512, 1024, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(1024),
-            nn.ReLU(inplace=True),
-            nn.Dropout3d(0.1),
+            nn.Conv3d(base_nc * 4, base_nc * 8, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm3d(base_nc * 8),
+            nn.ReLU(inplace=True), nn.Dropout3d(enc_dropout),
         )
 
-        self.bottleneck = nn.Sequential(
-            nn.Conv3d(1024, 1024, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm3d(1024),
-            nn.LeakyReLU(0.2),
-            nn.Dropout3d(0.2),
-
-            nn.Conv3d(1024, 1024, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm3d(1024),
-            nn.LeakyReLU(0.2),
-            nn.Dropout3d(0.2),
-
-            nn.Conv3d(1024, 1024, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm3d(1024),
-            nn.LeakyReLU(0.2),
-            nn.Dropout3d(0.2),
-        )
+        bottle_blocks = []
+        for _ in range(n_bottleneck):
+            bottle_blocks += [
+                nn.Conv3d(base_nc * 8, base_nc * 8, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm3d(base_nc * 8),
+                nn.LeakyReLU(0.2),
+                nn.Dropout3d(bottle_dropout),
+            ]
+        self.bottleneck = nn.Sequential(*bottle_blocks)
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose3d(1024, 512, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(512),
-            nn.ReLU(inplace=True),
-            nn.Dropout3d(0.1),
+            nn.ConvTranspose3d(base_nc * 8, base_nc * 4, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm3d(base_nc * 4),
+            nn.ReLU(inplace=True), nn.Dropout3d(dec_dropout),
 
-            nn.ConvTranspose3d(512, 256, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(256),
-            nn.ReLU(inplace=True),
-            nn.Dropout3d(0.1),
+            nn.ConvTranspose3d(base_nc * 4, base_nc * 2, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm3d(base_nc * 2),
+            nn.ReLU(inplace=True), nn.Dropout3d(dec_dropout),
 
-            nn.ConvTranspose3d(256, 128, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(128),
-            nn.ReLU(inplace=True),
-            nn.Dropout3d(0.1),
+            nn.ConvTranspose3d(base_nc * 2, base_nc, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm3d(base_nc),
+            nn.ReLU(inplace=True), nn.Dropout3d(dec_dropout),
 
-            nn.ConvTranspose3d(128, 1, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose3d(base_nc, 1, kernel_size=4, stride=2, padding=1),
             nn.Tanh()
         )
 
