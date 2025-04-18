75742a095a5fefe8dbf9c03f376d69d93869dbca
MMahdiSetak <m.mahdi.setak@gmail.com>
2025-04-18 14:58:52 +0330

    set hparam for Discriminator

--- model/gan/discriminator.py
+++ model/gan/discriminator.py
@@ -2,31 +2,45 @@ import torch.nn as nn
 
 
 class Discriminator(nn.Module):
-    def __init__(self):
+    def __init__(self,
+                 base_nf: int = 64,
+                 n_layers: int = 5,
+                 kernel_size: int = 4,
+                 stride: int = 2,
+                 padding: int = 1,
+                 use_bn: bool = True,
+                 dropout: float = 0.1, ):
         super(Discriminator, self).__init__()
-        self.model = nn.Sequential(
-            nn.Conv3d(1, 64, kernel_size=4, stride=1, padding=1),
+        layers = []
+        # first conv: no down‐sampling
+        layers += [
+            nn.Conv3d(1, base_nf, kernel_size=kernel_size, stride=1, padding=padding),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout3d(dropout),
+        ]
 
-            nn.Conv3d(64, 128, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(128),
-            nn.LeakyReLU(0.2, inplace=True),
+        # intermediate blocks
+        for i in range(1, n_layers):
+            in_ch = base_nf * (2 ** (i - 1))
+            out_ch = base_nf * (2 ** i)
+            layers += [
+                nn.Conv3d(in_ch, out_ch, kernel_size=kernel_size, stride=stride, padding=padding),
+            ]
+            if use_bn:
+                layers.append(nn.BatchNorm3d(out_ch))
+            layers += [
+                nn.LeakyReLU(0.2, inplace=True),
+                nn.Dropout3d(dropout),
+            ]
 
-            nn.Conv3d(128, 256, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(256),
-            nn.LeakyReLU(0.2, inplace=True),
+        # final conv → single‐channel “real/fake” score
+        last_ch = base_nf * (2 ** (n_layers - 1))
+        layers += [
+            nn.Conv3d(last_ch, 1, kernel_size=kernel_size, stride=1, padding=0),
+            nn.Sigmoid(),
+        ]
 
-            nn.Conv3d(256, 512, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(512),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            nn.Conv3d(512, 1024, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(1024),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            nn.Conv3d(1024, 1, kernel_size=4, stride=1, padding=0),
-            nn.Sigmoid()
-        )
+        self.model = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.model(x)
