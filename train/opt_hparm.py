5659a417c3706572203fff104299694a296c9ac2
MMahdiSetak <m.mahdi.setak@gmail.com>
2025-04-18 12:42:55 +0330

    optimize optuna

--- train/opt_hparm.py
+++ train/opt_hparm.py
@@ -1,22 +1,30 @@
-import optuna
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
-from torch import nn
+import torch
 from torch.utils.data import DataLoader
+import gc
 
 from model.cvit.model import SegmentTransformer
 from model.dataloader import MRIDataset
+
+batch_size = 256
+num_workers = 3
+train_loader = DataLoader(
+    dataset=MRIDataset('dataset/mri_label_v4.hdf5', 'train'),
+    batch_size=batch_size, num_workers=num_workers, shuffle=False, drop_last=False
+)
+val_loader = DataLoader(
+    dataset=MRIDataset('dataset/mri_label_v4.hdf5', 'val'),
+    batch_size=batch_size, num_workers=num_workers, shuffle=False, drop_last=False
+)
 
 
 def objective(trial):
     norm = trial.suggest_categorical('normalization', ['batch', 'instance', 'layer'])
     embedding_size = trial.suggest_int('embedding_size', 64, 512, step=4)
     dropout_rate = trial.suggest_float('dropout_rate', 0.1, 0.9)
-    batch_size = 128
-    num_workers = 3
 
     model = SegmentTransformer(embedding_size=embedding_size, dropout=dropout_rate, norm=norm, lr=1e-3)
-    model.classification_loss = nn.CrossEntropyLoss()
 
     trainer = pl.Trainer(
         max_epochs=30,
@@ -29,16 +37,10 @@ def objective(trial):
         enable_checkpointing=False
     )
 
-    train_loader = DataLoader(
-        dataset=MRIDataset('dataset/mri_label_v4.hdf5', 'train'),
-        batch_size=batch_size, num_workers=num_workers, shuffle=False, drop_last=False
-    )
-    val_loader = DataLoader(
-        dataset=MRIDataset('dataset/mri_label_v4.hdf5', 'val'),
-        batch_size=batch_size, num_workers=num_workers, shuffle=False, drop_last=False
-    )
-
     trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
     val_accuracy = trainer.callback_metrics['val_accuracy'].max()
+    del trainer, model
+    torch.cuda.empty_cache()
+    gc.collect()
     return val_accuracy
