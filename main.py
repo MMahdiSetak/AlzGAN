Fix imports, optimize interpolation, and update training precision

main.py
@@ -5,7 +5,7 @@ import torch
 
 from train.ddpm import run
 
-torch.set_float32_matmul_precision('high')
+torch.set_float32_matmul_precision('medium')
 run()
 
 # from dataset import mri_data_paths, create_mri_pet_label_dataset, pet_data_path

model/ddpm/modules.py
@@ -211,12 +211,7 @@ class Upsample(nn.Module):
 
     def forward(self, x):
         assert x.shape[1] == self.channels
-        if self.dims == 3:
-            x = F.interpolate(
-                x, (x.shape[2] * 2, x.shape[3] * 2, x.shape[4] * 2), mode="trilinear"
-            )
-        else:
-            x = F.interpolate(x, scale_factor=2, mode="trilinear")
+        x = F.interpolate(x, (x.shape[2] * 2, x.shape[3] * 2, x.shape[4] * 2), mode="trilinear")
         if self.use_conv:
             x = self.conv(x)
         return x