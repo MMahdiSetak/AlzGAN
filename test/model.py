5fa510a50c6d60d9a70dea4f3ebe3f88d41eb19c
MMahdiSetak <m.mahdi.setak@gmail.com>
2025-07-10 19:23:44 +0330

    Change the classifier model to 2018 paper

--- test/model.py
+++ test/model.py
@@ -8,35 +8,35 @@ class MRICNN(nn.Module):
 
         self.model = nn.Sequential(
             # CNN blocks
-            nn.Conv3d(1, 1, kernel_size=3, padding=1),
-            nn.BatchNorm3d(1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool3d(kernel_size=(2, 2, 3), stride=(2, 2, 3)),
-            nn.Dropout3d(0.2),
+            # nn.Conv3d(1, 1, kernel_size=3, padding=1),
+            # nn.BatchNorm3d(1),
+            # nn.ReLU(inplace=True),
+            # nn.MaxPool3d(kernel_size=(2, 2, 3), stride=(2, 2, 3)),
+            # nn.Dropout3d(0.2),
 
-            nn.Conv3d(1, 4, kernel_size=3, padding=1),
-            nn.BatchNorm3d(4),
-            nn.ReLU(inplace=True),
-            nn.MaxPool3d(kernel_size=(2, 2, 4), stride=(2, 2, 4)),
-            nn.Dropout3d(0.2),
+            # nn.Conv3d(1, 4, kernel_size=3, padding=1),
+            # nn.BatchNorm3d(4),
+            # nn.ReLU(inplace=True),
+            # nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2)),
+            # nn.Dropout3d(0.1),
 
-            nn.Conv3d(4, channels, kernel_size=3, padding=1),
+            nn.Conv3d(1, channels, kernel_size=3, padding=1),
             nn.BatchNorm3d(channels),
             nn.ReLU(inplace=True),
             nn.MaxPool3d(kernel_size=2, stride=2),
-            nn.Dropout3d(0.2),
+            # nn.Dropout3d(0.1),
 
             nn.Conv3d(channels, channels * 2, kernel_size=3, padding=1),
             nn.BatchNorm3d(channels * 2),
             nn.ReLU(inplace=True),
-            nn.MaxPool3d(kernel_size=2, stride=2),
-            nn.Dropout3d(0.2),
+            nn.MaxPool3d(kernel_size=3, stride=3),
+            # nn.Dropout3d(0.1),
 
             nn.Conv3d(channels * 2, channels * 4, kernel_size=3, padding=1),
             nn.BatchNorm3d(channels * 4),
             nn.ReLU(inplace=True),
-            # nn.MaxPool3d(kernel_size=2, stride=2),
-
+            nn.MaxPool3d(kernel_size=4, stride=4),
+            #
             # nn.Conv3d(channels * 4, channels * 8, kernel_size=3, padding=1),
             # nn.BatchNorm3d(channels * 8),
             # nn.ReLU(inplace=True),
@@ -66,6 +66,6 @@ class MRICNN(nn.Module):
         return self.model(x)
 
 
-classifier = MRICNN(num_classes=3, dropout_rate=0.3, channels=16).to('cuda')
+classifier = MRICNN(num_classes=3, dropout_rate=0.6, channels=32).to('cuda')
 
-summary(classifier, (1, 160, 160, 192))
+summary(classifier, (1, 160, 192, 160))
