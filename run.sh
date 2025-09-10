316a220fccf04e786b63205163217a60fd3694ee
MMahdiSetak <m.mahdi.setak@gmail.com>
2025-09-10 13:04:54 +0330

    run ablation studies (MRI+sPET)

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-python main.py augmentation=False tabular=False mri_dataset="dataset/mri_v5.2_SyN.hdf5"
+python main.py tabular=False
+python main.py augmentation=False tabular=False mri_dataset="dataset/mri_v5.2_Rigid.hdf5"
 python main.py augmentation=False tabular=False mri_dataset="dataset/mri_v5.2_Rigid.hdf5"
