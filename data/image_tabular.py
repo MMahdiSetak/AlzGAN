12a8039cfe01f39c5cdf7c4523291a5f24f32c0d
MMahdiSetak <m.mahdi.setak@gmail.com>
2025-09-11 23:22:17 +0330

    mci 2 year progress dataset creation

--- data/image_tabular.py
+++ data/image_tabular.py
@@ -471,6 +471,6 @@ def run():
     # merge_mri_csv('dataset/MRI2/ADNI/')
     # merge_mri_csv_pet('dataset/MRI2/ADNI/', 'dataset/PET/ADNI/')
     # create_mri_dataset()
-    create_mri_pet_dataset()
+    # create_mri_pet_dataset()
     # recreate_mri_dataset()
-    # create_mci_dataset()
+    create_mci_dataset()
