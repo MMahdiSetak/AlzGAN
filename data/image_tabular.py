5fbc842fcb182bd2c67bb9cf42cfb6dacad8246c
MMahdiSetak <m.mahdi.setak@gmail.com>
2025-09-12 17:36:04 +0330

    create all the progression datasets

--- data/image_tabular.py
+++ data/image_tabular.py
@@ -347,124 +347,130 @@ def recreate_mri_dataset():
 
 
 def create_mci_dataset():
-    df = pd.read_csv('dataset/tabular/img_merged.csv')
-    df['EXAMDATE'] = pd.to_datetime(df['EXAMDATE'], format='%Y-%m-%d')
-    mci = df[df['DIAGNOSIS'] == 1].copy().reset_index(drop=True)
-    df = df[df['DIAGNOSIS'] != 0]
-    stable = progress = no_data = 0
-    for index, row in mci.iterrows():
-        history = df[df['PTID'] == row['PTID']].sort_values(by='EXAMDATE')
-        history['delta_days'] = (history['EXAMDATE'] - row['EXAMDATE']).dt.days
-        filtered_history = history[(history['delta_days'] > 0) & (history['delta_days'] <= 183)].copy()
-        if filtered_history.empty:
-            no_data += 1
-            mci.loc[index, 'DIAGNOSIS'] = -1
-            continue
-        else:
-            mci.loc[index, 'DIAGNOSIS'] = filtered_history.iloc[-1]['DIAGNOSIS'] - 1
-            if filtered_history.iloc[0]['DIAGNOSIS'] == 1:
-                stable += 1
-            elif filtered_history.iloc[0]['DIAGNOSIS'] == 2:
-                progress += 1
-            else:
+    intervals = [
+        {'name': '2y', 'days': 730},
+        {'name': '1y', 'days': 365},
+        {'name': '6m', 'days': 183},
+    ]
+    for interval in intervals:
+        df = pd.read_csv('dataset/tabular/img_merged.csv')
+        df['EXAMDATE'] = pd.to_datetime(df['EXAMDATE'], format='%Y-%m-%d')
+        mci = df[df['DIAGNOSIS'] == 1].copy().reset_index(drop=True)
+        df = df[df['DIAGNOSIS'] != 0]
+        stable = progress = no_data = 0
+        for index, row in mci.iterrows():
+            history = df[df['PTID'] == row['PTID']].sort_values(by='EXAMDATE')
+            history['delta_days'] = (history['EXAMDATE'] - row['EXAMDATE']).dt.days
+            filtered_history = history[(history['delta_days'] > 0) & (history['delta_days'] <= interval['days'])].copy()
+            if filtered_history.empty:
                 no_data += 1
+                mci.loc[index, 'DIAGNOSIS'] = -1
+                continue
+            else:
+                mci.loc[index, 'DIAGNOSIS'] = filtered_history.iloc[-1]['DIAGNOSIS'] - 1
+                if filtered_history.iloc[0]['DIAGNOSIS'] == 1:
+                    stable += 1
+                elif filtered_history.iloc[0]['DIAGNOSIS'] == 2:
+                    progress += 1
+                else:
+                    no_data += 1
 
-    mci = mci[mci['DIAGNOSIS'] != -1].reset_index(drop=True)
+        mci = mci[mci['DIAGNOSIS'] != -1].reset_index(drop=True)
 
-    # ad_rows = history[history['DIAGNOSIS'] == 2]
-    # if ad_rows.empty:
-    #     no_data += 1
-    #     mci.loc[index, 'DIAGNOSIS'] = -1
-    #     continue
-    # first_ad_date = ad_rows['EXAMDATE'].min()
-    # delta = (first_ad_date - row['EXAMDATE']).days
-    #
-    # if delta <= 730:
-    #     progress += 1
-    #     # mci.iloc[index]['DIAGNOSIS'] = 1
-    # else:
-    #     stable += 1
-    #     mci.loc[index, 'DIAGNOSIS'] = 0
+        # ad_rows = history[history['DIAGNOSIS'] == 2]
+        # if ad_rows.empty:
+        #     no_data += 1
+        #     mci.loc[index, 'DIAGNOSIS'] = -1
+        #     continue
+        # first_ad_date = ad_rows['EXAMDATE'].min()
+        # delta = (first_ad_date - row['EXAMDATE']).days
+        #
+        # if delta <= 730:
+        #     progress += 1
+        #     # mci.iloc[index]['DIAGNOSIS'] = 1
+        # else:
+        #     stable += 1
+        #     mci.loc[index, 'DIAGNOSIS'] = 0
 
-    # if 2 in history['DIAGNOSIS'].values:
-    #     progress += 1
-    # else:
-    #     stable += 1
+        # if 2 in history['DIAGNOSIS'].values:
+        #     progress += 1
+        # else:
+        #     stable += 1
 
-    print(progress, stable, no_data)
-    df = mci
+        print(progress, stable, no_data)
+        df = mci
 
-    # df['DIAGNOSIS'] = df['DIAGNOSIS'] / 2
+        # df['DIAGNOSIS'] = df['DIAGNOSIS'] / 2
 
-    # exit(0)
-    # Define numerical cols for scaling
-    numerical_cols = ['MMSCORE', 'TOTSCORE', 'TOTAL13', 'FAQTOTAL', 'PTEDUCAT', 'AGE']
+        # exit(0)
+        # Define numerical cols for scaling
+        numerical_cols = ['MMSCORE', 'TOTSCORE', 'TOTAL13', 'FAQTOTAL', 'PTEDUCAT', 'AGE']
 
-    # First split: 80% train, 20% temp (val + test)
-    gss1 = GroupShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-    train_idx, temp_idx = next(gss1.split(df, groups=df['PTID']))
-    train = df.iloc[train_idx]
+        # First split: 80% train, 20% temp (val + test)
+        gss1 = GroupShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+        train_idx, temp_idx = next(gss1.split(df, groups=df['PTID']))
+        train = df.iloc[train_idx]
 
-    # Second split: 50% of temp for val (10% overall), 50% for test (10% overall)
-    temp_df = df.iloc[temp_idx]
-    gss2 = GroupShuffleSplit(n_splits=1, test_size=0.5, random_state=42)
-    val_idx_rel, test_idx_rel = next(gss2.split(temp_df, groups=temp_df['PTID']))
+        # Second split: 50% of temp for val (10% overall), 50% for test (10% overall)
+        temp_df = df.iloc[temp_idx]
+        gss2 = GroupShuffleSplit(n_splits=1, test_size=0.5, random_state=42)
+        val_idx_rel, test_idx_rel = next(gss2.split(temp_df, groups=temp_df['PTID']))
 
-    # Convert relative indices to absolute
-    val_idx = temp_df.iloc[val_idx_rel].index
-    test_idx = temp_df.iloc[test_idx_rel].index
+        # Convert relative indices to absolute
+        val_idx = temp_df.iloc[val_idx_rel].index
+        test_idx = temp_df.iloc[test_idx_rel].index
 
-    val = df.loc[val_idx]
-    test = df.loc[test_idx]
+        val = df.loc[val_idx]
+        test = df.loc[test_idx]
 
-    train = train.copy().reset_index(drop=True)
-    val = val.copy().reset_index(drop=True)
-    test = test.copy().reset_index(drop=True)
+        train = train.copy().reset_index(drop=True)
+        val = val.copy().reset_index(drop=True)
+        test = test.copy().reset_index(drop=True)
 
-    scaler = MinMaxScaler()
-    scaler.fit(train[numerical_cols])
-    train[numerical_cols] = scaler.transform(train[numerical_cols])
-    val[numerical_cols] = scaler.transform(val[numerical_cols])
-    test[numerical_cols] = scaler.transform(test[numerical_cols])
+        scaler = MinMaxScaler()
+        scaler.fit(train[numerical_cols])
+        train[numerical_cols] = scaler.transform(train[numerical_cols])
+        val[numerical_cols] = scaler.transform(val[numerical_cols])
+        test[numerical_cols] = scaler.transform(test[numerical_cols])
 
-    os.makedirs('dataset/img/', exist_ok=True)
-    for split_name, df in [('train', train), ('val', val), ('test', test)]:
-        df.to_csv(f'dataset/img/mci_{split_name}.csv', index=False)
-        y = df['DIAGNOSIS']
+        os.makedirs('dataset/img/', exist_ok=True)
+        for split_name, df in [('train', train), ('val', val), ('test', test)]:
+            df.to_csv(f'dataset/img/mci_{interval["name"]}_{split_name}.csv', index=False)
+            y = df['DIAGNOSIS']
 
-        # Print class distribution
-        print(f"\n{split_name.capitalize()} class distribution:")
-        print(y.value_counts())
+            # Print class distribution
+            print(f"\n{split_name.capitalize()} class distribution:")
+            print(y.value_counts())
 
-    mri_target = (160, 192, 160)
-    old_train_df = pd.read_csv('dataset/img/train.csv')
-    old_val_df = pd.read_csv('dataset/img/val.csv')
-    old_test_df = pd.read_csv('dataset/img/test.csv')
-    # Open old HDF5 once for reading
-    with h5py.File('dataset/mri_v5.2_Rigid.hdf5', 'r') as old_h5f:
-        with h5py.File('mci-6m_v5.2_Rigid.hdf5', 'w') as new_h5f:
-            ds = {
-                'mri_train': new_h5f.create_dataset('mri_train', (len(train), *mri_target), dtype='float32'),
-                'mri_val': new_h5f.create_dataset('mri_val', (len(val), *mri_target), dtype='float32'),
-                'mri_test': new_h5f.create_dataset('mri_test', (len(test), *mri_target), dtype='float32'),
-            }
-            for split, split_df in tqdm([('train', train), ('val', val), ('test', test)], desc="Processing splits"):
-                for index, row in tqdm(split_df.iterrows(), total=len(split_df), desc=f"Processing {split} rows"):
-                    image_path = row['image_path']
-                    match_train = old_train_df[old_train_df['image_path'] == image_path]
-                    match_val = old_val_df[old_val_df['image_path'] == image_path]
-                    match_test = old_test_df[old_test_df['image_path'] == image_path]
-                    if len(match_train) > 0:
-                        dataset_mri = old_h5f[f'mri_train'][match_train.index[0]]
-                    elif len(match_val) > 0:
-                        dataset_mri = old_h5f[f'mri_val'][match_val.index[0]]
-                    elif len(match_test) > 0:
-                        dataset_mri = old_h5f[f'mri_test'][match_test.index[0]]
-                    else:
-                        print(
-                            f"Warning: No unique match found for image_path '{image_path}' in old datasets. Skipping.")
-                        continue
-                    ds[f'mri_{split}'][index] = dataset_mri
+        mri_target = (160, 192, 160)
+        old_train_df = pd.read_csv('dataset/img/train.csv')
+        old_val_df = pd.read_csv('dataset/img/val.csv')
+        old_test_df = pd.read_csv('dataset/img/test.csv')
+        # Open old HDF5 once for reading
+        with h5py.File('dataset/mri_v5.2_Rigid.hdf5', 'r') as old_h5f:
+            with h5py.File(f'mci-{interval["name"]}_v5.2_Rigid.hdf5', 'w') as new_h5f:
+                ds = {
+                    'mri_train': new_h5f.create_dataset('mri_train', (len(train), *mri_target), dtype='float32'),
+                    'mri_val': new_h5f.create_dataset('mri_val', (len(val), *mri_target), dtype='float32'),
+                    'mri_test': new_h5f.create_dataset('mri_test', (len(test), *mri_target), dtype='float32'),
+                }
+                for split, split_df in tqdm([('train', train), ('val', val), ('test', test)], desc="Processing splits"):
+                    for index, row in tqdm(split_df.iterrows(), total=len(split_df), desc=f"Processing {split} rows"):
+                        image_path = row['image_path']
+                        match_train = old_train_df[old_train_df['image_path'] == image_path]
+                        match_val = old_val_df[old_val_df['image_path'] == image_path]
+                        match_test = old_test_df[old_test_df['image_path'] == image_path]
+                        if len(match_train) > 0:
+                            dataset_mri = old_h5f[f'mri_train'][match_train.index[0]]
+                        elif len(match_val) > 0:
+                            dataset_mri = old_h5f[f'mri_val'][match_val.index[0]]
+                        elif len(match_test) > 0:
+                            dataset_mri = old_h5f[f'mri_test'][match_test.index[0]]
+                        else:
+                            print(
+                                f"Warning: No unique match found for image_path '{image_path}' in old datasets. Skipping.")
+                            continue
+                        ds[f'mri_{split}'][index] = dataset_mri
 
 
 def run():
